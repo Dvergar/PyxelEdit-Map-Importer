c8915f7f476d7e7ac5ddd1bd0133ac0c98278c13
Jonathan Hirz <jonathanhirz@gmail.com>
2015-06-07 23:11:28 -0700

    updating resource loading calls

--- tests/luxe/src/Main.hx
+++ tests/luxe/src/Main.hx
@@ -2,16 +2,26 @@ import pmi.PyxelMapImporter;
 import pmi.LuxeHelper;
 
 
-class Main extends luxe.Game
-{
+class Main extends luxe.Game {
+
+    override function config(config:luxe.AppConfig) {
+
+        config.preload.texts.push({ id:'assets/map-paid.xml' });
+        config.preload.texts.push({ id:'assets/map-free.xml' });
+        config.preload.textures.push({ id:'assets/tileset.png' });
+
+        return config;
+        
+    } //config
+
     override function ready()
     {
         #if paid
             trace("TESTING PAID VERSION");
-            var pyxelMap = new PyxelMapImporter(Luxe.loadText("assets/map-paid.xml", null, false).text);
+            var pyxelMap = new PyxelMapImporter(Luxe.resources.text('assets/map-paid.xml').asset.text);
         #else
             trace("TESTING FREE VERSION");
-            var pyxelMap = new PyxelMapImporter(Luxe.loadText("assets/map-free.xml", null, false).text);
+            var pyxelMap = new PyxelMapImporter(Luxe.resources.text('assets/map-free.xml')asset.text);
         #end
 
     	var tilemap = LuxeHelper.getTilemap('assets/tileset.png');
